148e4722ae931ddd5703a5d839804e89255aa749
riazalmahmud <riazalmahmud041@gmail.com>
2025-06-22 09:37:40 +0600

    temperature  converter

--- learn-csharp/Program.cs
+++ learn-csharp/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
-class Test
+class TemperatureConverter
 {
+
+
     public static void Main(string[] args)
     {
-        // double fahrenheit, calicos;
-        // Console.Write("Enter the temperature in Celsius: ");
-        // fahrenheit = Convert.ToDouble(Console.ReadLine());
-        // calicos = (fahrenheit - 32) % 1.8;
-        // Console.WriteLine("The temperature in Fahrenheit is: " + calicos.ToString("F2"));
+        Console.WriteLine("Enter temperature in Celsius:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 1)
+        {
+            Console.WriteLine("Enter temperature in Fahrenheit:");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit -32) /1.8;
+            Console.WriteLine($"Temperature in celsius: {celsius}");
+        }
+        else if (input == 2)
+        {
+            Console.WriteLine("Enter temperature in celsius:");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = 1.8 * celsius + 32;
+            Console.WriteLine($"Temperature in Celsius: {fahrenheit}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter 1 for Celsius to Fahrenheit or 2 for Fahrenheit to Celsius.");
+        }
 
-        double fahrenheit, celsius;
-        Console.Write("Enter the temperature in Celsius: ");
-        celsius = Convert.ToDouble(Console.ReadLine());
-        fahrenheit = (1.8 * celsius) + 32;
-        Console.WriteLine("The temperature in Fahrenheit is: " + fahrenheit.ToString("F2"));
     }
 }
 
